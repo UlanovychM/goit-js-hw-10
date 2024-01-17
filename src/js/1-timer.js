f36502e9063136339c0218ea9817791c1fb0c4b8
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-01-17 09:13:12 +0200

    end snackbar, try do timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,63 @@
 'use strict';
+import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
+
+const pickrTime = document.querySelector('#datetime-picker');
+const button = document.querySelector('#data-start');
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates <= this.defaultDate) {
+      window.alert('Please choose a date in the future');
+    }
+    selectedDates[0];
+
+    // button.classList.add('btn-disable');
+  },
+};
+
+console.log(options.onClose(userSelectedDate));
+
+flatpickr(pickrTime, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// button.addEventListener('click', e => {
+//   console.log(options.onClose(e.target));
+// });
+
+function getZero(num) {
+  if (num >= 0 && num < 10) {
+    return `0${num}`;
+  } else {
+    return num;
+  }
+}
+
+const changeDateInTimer = () => {
+  const days = document.querySelector('#data-days');
+  const hours = document.querySelector('#data-hours');
+  const minutes = document.querySelector('#data-minutes');
+  const seconds = document.querySelector('#data-seconds');
+};
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,52 @@
 'use strict';
+
+import iziToast from 'izitoast';
+
+const form = document.querySelector('.form');
+let userData = {};
+const localStorageKey = 'feedback-form-state';
+
+// const promise = new Promise();
+
+form.addEventListener('input', e => {
+  if (e.target.value !== '' && e.target.value !== null) {
+    userData[e.target.name] = e.target.value.trim();
+    localStorage.setItem(localStorageKey, JSON.stringify(userData));
+  }
+  return;
+});
+
+if (localStorage.getItem(localStorageKey)) {
+  userData = JSON.parse(localStorage.getItem(localStorageKey));
+
+  form.elements.delay.value = userData.delay;
+  form.elements.state.value = userData.state;
+}
+
+const makePromise = ({ delay, state }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      }
+      reject(delay);
+    }, delay);
+  });
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  makePromise(userData)
+    .then(delay =>
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      })
+    )
+    .catch(delay =>
+      iziToast.error({ message: `❌ Rejected promise in ${delay}ms` })
+    );
+
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
